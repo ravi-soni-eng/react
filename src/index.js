211e968a9e027d19f31a45de67d24df2aa762dc0
ravi-soni-eng <ravi143soni143@gmail.com>
2021-01-01 19:33:48 -0800

    react tutorila hello

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Football(params) {
+  return(
+  <h1>hello friends ,
+    "welcome to my youtube channel";
+  </h1>
+  )
+  
+
+}
+
+
+
+ReactDOM.render(<Football />, document.getElementById('root'));
